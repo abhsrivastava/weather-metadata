930c46213a36f9a388a55148ae0ac1b045d49a3a
Abhishek Srivastava <abhishes@gmail.com>
2023-04-16 23:18:36 -0700

    got Lwt Throttling to work

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,10 @@ open Weather_metadata.Weather
 open Weather_metadata.Data
 
 let () = 
-  get_metadata() 
+  let json = get_metadata() 
   |> Lwt.map Metadata.to_json_list
   |> Lwt_main.run
-  |> Yojson.Basic.pretty_to_string
-  |> print_endline
+  |> Yojson.Basic.pretty_to_string in 
+  let file = open_out "trail_database.json" in 
+  json |> Printf.fprintf file "%s\n";
+  file |> close_out
--- lib/weather.ml
+++ lib/weather.ml
@@ -3,64 +3,90 @@ open Lwt
 open Cohttp_lwt_unix
 open Data
 
+module RateLimiter = Lwt_throttle.Make(
+  struct
+    type t = string
+    let hash = Hashtbl.hash
+    let equal s1 s2 = hash s1 == hash s2
+  end
+)
+
 let buildUrl (lat, long) : string =
-  "https://api.weather.gov/points/" ^ (Util.to_string lat) ^ "," ^ (Util.to_string long) |> String.trim
+  "https://api.weather.gov/points/" ^ 
+  (Util.to_string lat) ^ "," ^ 
+  (Util.to_string long) |> String.trim
 
-let make_http_call ?(wait=15) = function 
-  | Process(list) -> 
-    list 
-    |> List.map(Uri.of_string)
-    |> Lwt_list.map_s (fun uri -> 
-      Lwt.catch 
-        (fun () -> uri |> Client.get |> Lwt.map(Option.some)) 
-        (function 
-        | exn -> "\nReceived Exception for url: " ^ (uri |> Uri.to_string) ^ "\nException: " ^ (exn |> Printexc.to_string) |> Lwt_io.(write stdout) |> Lwt.map(fun _ -> None)
-        )
-    )
-    |> Lwt.map(Util.remove_none)
-    >>= (fun list -> 
-      list |> Lwt_list.map_s(fun (resp, body) -> 
-        let code = resp |> Response.status |> Cohttp.Code.code_of_status in
-        if code == 200 then 
-          Lwt.catch 
-            (fun () -> body |> Cohttp_lwt.Body.to_string |> Lwt.map (Option.some))
-            (function
-              | exn -> exn |> Printexc.to_string |> Lwt_io.(write stdout) |> Lwt.map ( fun _ -> None)
-            )
-        else  
-          "did not get 200 response" |> Lwt_io.(write stdout) |> Lwt.map ( fun _ -> None )
+let make_http_call client uriStr =
+    let uri = uriStr |> Uri.of_string in
+    Lwt.catch 
+      (fun () -> uri |> client |> Lwt.map(Option.some)) 
+      (function 
+      | exn -> 
+        "\nReceived Exception for url: " ^ 
+        (uri |> Uri.to_string) ^ 
+        "\nException: " ^ (exn |> Printexc.to_string) 
+        |> Lwt_io.(write stdout) 
+        |> Lwt.map(fun _ -> None)
       )
+    >>= (
+      function
+      | Some(resp, body) -> 
+          let code = resp |> Response.status |> Cohttp.Code.code_of_status in
+          if code == 200 then 
+            Lwt.catch 
+              (fun () -> body |> Cohttp_lwt.Body.to_string |> Lwt.map (Option.some))
+              (function
+              | exn -> exn |> Printexc.to_string |> Lwt_io.(write stdout) |> Lwt.map ( fun _ -> None)
+              )
+          else
+            "did not get 200 response" 
+            |> Lwt_io.(write stdout) 
+            |> Lwt.map ( fun _ -> None )  
+      | None -> () |> Lwt.return |> Lwt.map(fun _ -> None)  
     )
-  | Wait -> 
-    print_endline("going to wait now for " ^ (wait |> Int.to_string) ^ " seconds");
-    Unix.sleep(wait); 
-    [None] |> Lwt.return
 
-let get_metadata_for_trails () =
+let get_metadata_for_trails client =
   trailsList 
   |> List.map buildUrl
-  |> Util.split 10
-  |> Lwt_list.map_s(make_http_call) (* returns string option list list Lwt :) *)
-  |> Lwt.map(List.flatten) (* now we are down to string option list lwt thanks to flatten *)
+  |> Lwt_list.map_s(make_http_call client) (* returns string option list Lwt *)
   |> Lwt.map (Util.remove_none) (* now we are down to string list lwt thanks to remove none *)
   
 let parse_json json = 
+  try 
   Metadata.(
     Yojson.Basic.Util.(
       {
-        gridId = json |> member "properties" |> member "gridId" |> to_string;
-        gridX = json |> member "properties" |> member "gridX" |> to_int;
-        gridY = json |> member "properties" |> member "gridY" |> to_int;
-        forecastHourly = json |> member "properties" |> member "forecastHourly" |> to_string;
-        forecastZone = json |> member "properties" |> member "forecastZone" |> to_string;
-        county = json |> member "properties" |> member "county" |> to_string;
-        radarStation = json |> member "properties" |> member "radarStation" |> to_string;
-        city = json |> member "properties" |> member "relativeLocation" |> member "properties" |> member "city" |> to_string;
-      }
+        gridId = json |> member "properties" |> member "gridId" |> to_string_option |> Option.value ~default:"";
+        gridX = json |> member "properties" |> member "gridX" |> to_int_option |> Option.value ~default:0;
+        gridY = json |> member "properties" |> member "gridY" |> to_int_option |> Option.value ~default:0;
+        forecastHourly = json |> member "properties" |> member "forecastHourly" |> to_string_option |> Option.value ~default:"";
+        forecastZone = json |> member "properties" |> member "forecastZone" |> to_string_option |> Option.value ~default:"";
+        county = json |> member "properties" |> member "county" |> to_string_option |> Option.value ~default:"";
+        radarStation = json |> member "properties" |> member "radarStation" |> to_string_option |> Option.value ~default:"";
+        city = json |> member "properties" |> member "relativeLocation" |> member "properties" |> member "city" |> to_string_option |> Option.value ~default:"";
+      } |> Option.some
     )
   )
+  with 
+  | exn -> 
+    "Failed to parse json: \n" ^ (json |> Yojson.Basic.pretty_to_string) ^
+    "Exception: \n" ^ (exn |> Printexc.to_string) ^ "\n"
+    |> print_endline; 
+    None
 
 let get_metadata () = 
-  get_metadata_for_trails() 
+  let rec throttled_get uri = 
+    let limiter = RateLimiter.create ~rate:1 ~max:1 ~n:1 in 
+    let channel = "https://api.weather.gov/" in   
+    let open Lwt.Syntax in 
+    let* allowed = RateLimiter.wait limiter channel in
+    if allowed then 
+      uri |> Client.get 
+    else 
+      let* () = Lwt_unix.sleep 1. in throttled_get uri
+  in
+  throttled_get 
+  |> get_metadata_for_trails 
   |> Lwt.map (List.map Yojson.Basic.from_string)
   |> Lwt.map (List.map parse_json)
+  |> Lwt.map(Util.remove_none)
